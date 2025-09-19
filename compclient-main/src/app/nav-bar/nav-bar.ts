f5ddfd481226cc4c8c2197289a68f87347ecdfbc
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-09-19 16:44:47 -0700

    weather app files

---
+++ compclient-main/src/app/nav-bar/nav-bar.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss'
+})
+
+export class NavBar {
+
+}
