be5a229c0d1b3e6e473a5b51dc4d5de3b117a122
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-09-26 18:08:50 -0700

    database creation

---
+++ comp-584-sever-main/WorldModel/City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("city")]
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("countryid")]
+    public int Countryid { get; set; }
+
+    [Column("name")]
+    [StringLength(10)]
+    public string Name { get; set; } = null!;
+
+    [Column("lat")]
+    public int Lat { get; set; }
+
+    [Column("long")]
+    public int Long { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [ForeignKey("Countryid")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ comp-584-sever-main/WorldModel/DatabasedContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+public partial class DatabasedContext : DbContext
+{
+    public DatabasedContext()
+    {
+    }
+
+    public DatabasedContext(DbContextOptions<DatabasedContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=(localdb)\\mssqllocaldb;initial catalog=databasedgolden");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.Property(e => e.Name).IsFixedLength();
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Cities)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_city_country");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.Property(e => e.Iso2).IsFixedLength();
+            entity.Property(e => e.Iso3).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
