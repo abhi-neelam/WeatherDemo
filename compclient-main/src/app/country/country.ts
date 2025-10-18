1e94385812cb61f4d76d53fef0f80c9b590e1d68
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-10-17 17:47:38 -0700

    add environments and create ui for countries

--- compclient-main/src/app/country/country.ts
+++ compclient-main/src/app/country/country.ts
@@ -1,11 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CountryData } from './country-data';
+import { environment } from '../../environments/environment.development';
 
 @Component({
-  selector: 'app-country',
-  imports: [],
-  templateUrl: './country.html',
-  styleUrl: './country.scss'
+    selector: 'app-country',
+    imports: [],
+    templateUrl: './country.html',
+    styleUrl: './country.scss'
 })
-export class Country {
 
+export class Country {
+    countries: CountryData[] = [];
+
+    constructor(http: HttpClient) {
+        http.get<CountryData[]>(environment.apiUrl + "/api/countries").subscribe(result => {
+            this.countries = result;
+        });
+    }
 }
